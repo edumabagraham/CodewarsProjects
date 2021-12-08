e7cbcaf8e7860540344ee614e227d688ae22b38a
edumabagraham <edumabagrahamp@gmail.com>
2021-12-08 10:55:21 +0000

    Break camelCase- COMPLETE

--- 6kyu/break_camel_case.py
+++ 6kyu/break_camel_case.py
@@ -1,11 +1,60 @@
-# def solution(string):
+# Complete the solution so that the function
+# will break up camel casing, using a space between words.
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
+
+# Solution 1
+def solution(s):
+    bldrs = [[s[0]]]  # Store first character
+    for c in s[1:]:
+        if bldrs[-1][-1].islower() and c.isupper():
+            bldrs.append([c])
+        else:
+            bldrs[-1].append(c)
+    joinedString = [''.join(bldr) for bldr in bldrs]
+
+    # Convert to string using join
+    return ' '.join(joinedString)
+
+# # Convert to string using list comprehension
+# print(' '.join([i for i in joinedString]))
 #
+# # Convert to string using map
+# print(' '.join(map(str, joinedString)))
+print(solution('camelCase'))
 
 
-str = 'camelCase'
-str2 = []
+# Solution 2
+def solution(s):
+    newStr = ""
+    for letter in s:
+        if letter.isupper():
+            newStr += " "
+        newStr += letter
+    return newStr
 
-for i in str:
-    str2.append(i)
+# Solution 3
+def solution(s):
+    return ''.join(' ' + c if c.isupper() else c for c in s)
 
-print(str2)
+print(solution('camelCase'))
+
+
+
+
+
+
+
+
+
+
+
+# import re
+# name = 'camelCaseT'
+# splitted = re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', name)).split()
+# print(splitted)
+
+
+
+
--- 6kyu/highest_scoring_word.py
+++ 6kyu/highest_scoring_word.py
@@ -1,28 +1,32 @@
+import time
+start = time.time()
+
 dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12,
         'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24,
         'y': 25, 'z': 26}
 
-str = 'take me to semynak'
+str = 'what time are we climbing up the volcano'
 str = str.split(' ')
-print(str)
-count = 0
-highest_count = 0
 
 
 
-for i in str:
+def high(x):
+    count = 0
+    highest_count = 0
+    for i in x:
+        for j in i:
+            count += dict[j]
 
-    for j in i:
-        count += dict[j]
-
-    if (count > highest_count):
-        highest_count = count
-        string = i
-        count = 0
-    else:
-        count = 0
+        if (count > highest_count):
+            highest_count = count
+            string = i
+            count = 0
+        else:
+            count = 0
+    return string
 
 
+print(high(str))
 
-print(highest_count)
-print(string)
+end = time.time()
+print('Execution time is: ', end-start)
